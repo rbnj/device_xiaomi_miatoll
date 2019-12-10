4cbfcd5f25f1280cd2480aa39025beee24b8b5ff
justinweng <justinweng@google.com>
2019-12-10 18:31:56 +0800

    miatoll: audio: Enforce 24-bit audio for offload playback
    Test: Audio Playback
    
    Change-Id: Ia581e65a0e5e8dcbfae188fe36ca9d07bd9eb4e2
    Signed-off-by: justinweng <justinweng@google.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -56,9 +56,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
     audio.offload.min.duration.secs=30 \
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.config.vc_call_vol_steps=10
-
-PRODUCT_VENDOR_PROPERTIES += \
+    persist.vendor.audio_hal.dsp_bit_width_enforce_mode=24 \
+    ro.config.vc_call_vol_steps=10 \
     ro.vendor.audio.spk.clean=true \
     ro.vendor.audio.soundfx.type=mi \
     ro.vendor.audio.vocal.support=false \
