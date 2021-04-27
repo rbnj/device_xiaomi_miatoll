365fab4df30095b1dc8c25db0eb1f095e753d17c
Alexander Koskovich <zvnexus@outlook.com>
2021-04-27 06:57:55 -0700

    miatoll: Remove virtual framebuffer.
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,8 +115,7 @@ BOARD_KERNEL_CMDLINE += \
     msm_rtb.enabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
-    swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    swiotlb=1
 
 # OTA
 TARGET_OTA_ASSERT_DEVICE := curtana,excalibur,gram,joyeuse,miatoll
