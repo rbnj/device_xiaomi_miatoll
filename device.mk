3fd7e02f19a950da880ddf71dbefc406912824cf
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-05 15:56:14 +0530

    miatoll: Import pixel power HAL from hardware/xiaomi
    - Remove namespace guards, untrack hardware/xiaomi
    - Remove power mode extension because we have in-tree power hal
    - Remove ndk_platform backend
    - Based on implementation from:
      https://github.com/LineageOS/android_hardware_xiaomi/tree/lineage-18.1/aidl/power-libperfmgr
    
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- device.mk
+++ device.mk
@@ -315,7 +315,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/xiaomi \
     vendor/qcom/opensource/commonsys-intf/display
 
 # Netflix
