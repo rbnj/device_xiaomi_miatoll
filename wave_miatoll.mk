262e3f82175e749501e511b0d56affc1520611c0
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-02-08 02:22:08 +0600

    miatoll: Until we meet again
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>
    Change-Id: I1266d29b288d23ab3f616c9eb3927ec75e1d1408

--- wave_miatoll.mk
+++ wave_miatoll.mk
@@ -1,19 +1,25 @@
+#
+# Copyright (C) 2021 Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from miatoll device
-$(call inherit-product, device/xiaomi/miatoll/atoll.mk)
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 # Inherit some common WaveOS stuff.
 $(call inherit-product, vendor/wave/configs/common.mk)
 
 # Device identifier
-PRODUCT_NAME := wave_miatoll
-PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Miatoll
+PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Miatoll
+PRODUCT_NAME := wave_miatoll
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Bootanimation
