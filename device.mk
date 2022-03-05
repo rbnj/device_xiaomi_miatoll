38e1ecbc8b54ca5c81ecf0911f3016bdbdc867f3
BladeRunner-A2C <prisoner56898@protonmail.com>
2022-03-06 00:46:06 +0600

    miatoll: lights: Rebrand to miatoll
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- device.mk
+++ device.mk
@@ -279,7 +279,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_atoll
+    android.hardware.lights-service.xiaomi_miatoll
 
 # Media
 PRODUCT_PACKAGES += \
