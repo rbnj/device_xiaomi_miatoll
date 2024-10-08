6198c628bd73b0c0099a28c4233b8e4900dea2c7
bengris32 <bengris32@protonmail.ch>
2024-10-08 07:29:59 +0000

    miatoll: camera: Implement setTorchModeExt
    Change-Id: Id61420be75b7efd1d13a4b0ee1d103ebd3835516
    Signed-off-by: electimon <electimon@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -73,3 +73,8 @@
      if (torchStrength > 0)
          set(TOGGLE_SWITCH, 255);
  }
+
+ void setTorchModeExt(bool enabled) {
+    int32_t strength = getTorchDefaultStrengthLevelExt();
+    setTorchStrengthLevelExt(enabled ? strength : 0);
+}
