6c55b51626a2556dceef54fb07b948f2a70d9d28
aswin7469 <aswinas@pixysos.com>
2025-01-29 01:45:25 +0530

    [REVERTME] miatoll: Don't enforce kernel vintf checks for now
    - also mark UFFD GC disabled

--- miatoll.mk
+++ miatoll.mk
@@ -204,6 +204,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Kernel
+PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS := false
+PRODUCT_ENABLE_UFFD_GC := false
+
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor \
