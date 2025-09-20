a76dd9cfeef01d9e22c407ceddc7c7606e80429f
LuK1337 <priv.luk@gmail.com>
2025-09-20 16:26:01 +0530

    miatoll: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- miatoll.mk
+++ miatoll.mk
@@ -34,9 +34,8 @@ PRODUCT_NAME := miatoll
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="miatoll_global-user 12 SKQ1.211019.001 V14.0.3.0.SJZMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/miatoll_global/miatoll:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys
+    BuildDesc="miatoll_global-user 12 SKQ1.211019.001 V14.0.3.0.SJZMIXM release-keys" \
+    BuildFingerprint=Redmi/miatoll_global/miatoll:12/RKQ1.211019.001/V14.0.3.0.SJZMIXM:user/release-keys
 
 # Boot animation resolution.
 TARGET_BOOT_ANIMATION_RES := 1080
