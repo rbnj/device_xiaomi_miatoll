974e9b0241066234ca4a6e30840d2b82001acdca
Alexander Koskovich <akoskovich@pm.me>
2025-11-08 17:18:57 +0530

    miatoll: Opt out of VIDEO_ENCODE flag for DeviceAsWebcam
    DeviceAsWebcam is using USAGE_VIDEO_ENCODE [1] for buffers and
    this results in combined usage: (VIDEO_ENCODER | CAMERA_OUTPUT).
    
    On Qcom devices DeviceAsWebcam has red & blue colors swapped.
    
    Originally we tried swapping HAL_PIXEL_FORMAT_YCbCr_420_SP_VENUS
    with HAL_PIXEL_FORMAT_YCrCb_420_SP_VENUS in libgrallocutils,
    however that inverted regular camera recording colors in some cases.
    
    [1] https://android.googlesource.com/platform/packages/services/DeviceAsWebcam/+/d079a82ba4f773210c01d488d2dfa1d4d25be8e2
    
    Change-Id: I6c5f03fe7016f687855351c7ba15270050ea02e2

--- device.mk
+++ device.mk
@@ -463,6 +463,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 endif
 
 PRODUCT_VENDOR_PROPERTIES += \
+    ro.usb.uvc.disable_video_encode_flag=true \
     ro.usb.uvc.enabled=true
 
 PRODUCT_HAS_GADGET_HAL := true
